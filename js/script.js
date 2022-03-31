fda2d9952afb55ff19f7d604babd7db4e9dd7c9e
lee-o <oconnor.sherri@gmail.com>
2022-03-31 12:45:54 -0400

    Changed html formatting

--- js/script.js
+++ js/script.js
@@ -13,6 +13,17 @@ profileFetch();
 
 const userInfo = function (data) {
     const userStats = document.createElement("div");
-    userStats.innerHTML = `<figure><img alt="user avatar" src=${data.avatar_url} /></figure><div class="user-info"><p><strong>Name:</strong> ${data.name}</p><p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p><p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`;
+    userStats.classList.add("user-info");
+    userStats.innerHTML = `
+    <figure>
+        <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+    `;
     overview.append(userStats);
 };
